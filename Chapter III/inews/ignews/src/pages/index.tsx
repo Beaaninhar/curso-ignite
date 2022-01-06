9a30e1690cebe262d001414fdde8b83b9f6e9d9d
Beaaninhar <aninhabeatrizribeiro@gmail.com>
2022-01-06 20:53:58 -0300

    Componentes e paginas - Fundamentos next - Chapter III

--- Chapter III/inews/ignews/src/pages/index.tsx
+++ Chapter III/inews/ignews/src/pages/index.tsx
@@ -1,12 +1,27 @@
 import Head from 'next/head'
+import { SubscribeButton } from '../components/SubscribeButton';
+import styles from './home.module.scss';
 
 export default function Home() {
   return (
     <>
-    <Head>
-      <title>HOME | ig.news</title>
-    </Head>
-      <h1>Olá, Mundo<span>!</span></h1>
+      <Head>
+        <title>HOME | IG.NEWS</title>
+      </Head>
+      
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏 Hey! Welcome</span>
+          <h1>News about the <span>React</span> world</h1>
+          <p>
+              Get acess to all the publications <br />
+              <span>for $9.90 month</span>
+            </p>
+            <SubscribeButton />
+        </section>
+
+        <img src="/images/avatar.svg" alt="Girl coding" />
+      </main>
     </>
   )
 }
