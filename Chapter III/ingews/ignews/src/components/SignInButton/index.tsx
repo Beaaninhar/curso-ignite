1861f8f03a66f9ab4ec42bbdb8e88f9908ba18c0
Beaaninhar <aninhabeatrizribeiro@gmail.com>
2022-01-20 15:37:39 -0300

    Feito API Routes no Next.js - MODULO Back-end no Front-end - Chapter III

--- Chapter III/ingews/ignews/src/components/SignInButton/index.tsx
+++ Chapter III/ingews/ignews/src/components/SignInButton/index.tsx
@@ -1,20 +1,29 @@
 import { FaGithub } from 'react-icons/fa';
 import { FiX } from 'react-icons/fi';
+import { signIn, signOut, useSession } from 'next-auth/react';
 
 import styles from './styles.module.scss';
 
 export function SingInButton() {
-  const isUserLoggedIn = true;
-  return isUserLoggedIn ? (
-    <button type="button" className={styles.singInButton}>
-      <FaGithub color='#04d361'/>
-      Ana Beatriz
-      <FiX color='#737380' className={styles.closeIcon}/>
-    </button>
-  ) : (
-    <button type="button" className={styles.singInButton}>
-      <FaGithub color='#eba417'/>
-      Sign in with Github
-    </button>
-  )
-};
+  const { data: session } = useSession()
+
+    return session ? (
+        <button type="button"
+        className={styles.singInButton}
+        onClick={() => signOut()}
+        >
+            <FaGithub color="#04d361"/>
+            {session.user.name}
+            <FiX color="#737380" className={styles.closeIcon} />
+        </button>
+    ) : (
+        <button type="button"
+        className={styles.singInButton}
+        onClick={() => signIn('github')}
+        >
+            <FaGithub color="#eba417"/>
+            Sign in with Github
+        </button>
+    )
+
+}  
